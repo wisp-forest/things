cd85baa9edd877ec4259719ee5fe0308400a82e7
glisco <glisco03@web.de>
2021-08-21 19:20:41 +0200

    Fix retribution applying to all kinds of shit

---
+++ src/main/java/com/glisco/things/mixin/EnchantmentHelperMixin.java
@@ -0,0 +1,21 @@
+package com.glisco.things.mixin;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.EnchantmentLevelEntry;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(EnchantmentHelper.class)
+public class EnchantmentHelperMixin {
+
+    @Inject(method = "getPossibleEntries", at = @At("RETURN"))
+    private static void removeInvalidEnchantments(int power, ItemStack stack, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
+        cir.getReturnValue().removeIf(enchantmentLevelEntry -> !enchantmentLevelEntry.enchantment.isAcceptableItem(stack));
+    }
+
+}
