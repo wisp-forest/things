bd3574fcf36d8ffd20d1f2c3095859768f816a76
glisco <glisco03@web.de>
2022-01-12 23:37:03 +0100

    change step height mixin to avoid redirecting

--- src/main/java/com/glisco/things/mixin/EntityMixin.java
+++ src/main/java/com/glisco/things/mixin/EntityMixin.java
@@ -3,19 +3,36 @@ package com.glisco.things.mixin;
 import com.glisco.things.Things;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import org.objectweb.asm.Opcodes;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public class EntityMixin {
 
-    @Redirect(at = @At(value = "FIELD", target = "Lnet/minecraft/entity/Entity;stepHeight:F", opcode = Opcodes.GETFIELD), method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;")
-    public float getStepHeight(Entity e) {
-        if (!(e instanceof PlayerEntity player)) return e.stepHeight;
-        if (!Things.SOCK_DATA.get(player).jumpySocksEquipped) return e.stepHeight;
-        return e.stepHeight + .5f;
+    @Shadow public float stepHeight;
+
+    @Unique private float things$prevStepHeight = -1;
+
+    @Inject(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;", at = @At("HEAD"))
+    private void boostStepHeight(Vec3d movement, CallbackInfoReturnable<Vec3d> cir) {
+        if (!((Object) this instanceof PlayerEntity player)) return;
+        if (!Things.SOCK_DATA.get(player).jumpySocksEquipped) return;
+
+        this.things$prevStepHeight = this.stepHeight;
+        this.stepHeight = this.stepHeight + .45f;
+    }
+
+    @Inject(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;", at = @At("RETURN"))
+    private void resetStepHeight(Vec3d movement, CallbackInfoReturnable<Vec3d> cir) {
+        if (this.things$prevStepHeight == -1) return;
+
+        this.stepHeight = this.things$prevStepHeight;
+        this.things$prevStepHeight = -1;
     }
 
 }
